772bdcd3e6581c1ff945473542975941803d2b2d
Vaibhav Tamakuwala <vaibhavtamakuwala.btce2018@iar.ac.in>
2022-01-01 13:07:13 +0530

    Add files via upload

---
+++ ams-micro-app/src/app/integration/integration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kykz-integration',
+  templateUrl: './integration.component.html',
+  styleUrls: ['./integration.component.scss']
+})
+export class IntegrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
